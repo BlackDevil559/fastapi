890ee26818e3f758eb709efd046f80fb62975853
Bhumesh Gaur <113198254+BlackDevil559@users.noreply.github.com>
2024-01-08 11:26:25 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,3 @@
+from datagovindia import DataGovIndia
+datagovin = DataGovIndia("579b464db66ec23bdd0000019b19496dc32649b35ab212cccf54971a") # Specify API key if not set as an environment variable
+datagovin.sync_metadata()
